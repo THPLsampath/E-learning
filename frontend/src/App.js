55192bd99eca62ffc01caa58de5c01caa969b920
THPLsampath <sampaththpl@gmail.com>
2022-05-13 12:14:15 +0530

    change app.js data.js index.js product.js rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import data from './data';
+import Product from './components/Product'
+
 
 function App() {
   return (
@@ -18,24 +20,7 @@ function App() {
             <div className="row center">
               {
                 data.products.map(product=>(
-                  <div key={product._id} className="card">
-                    <a href={`/product/${product._id}`}>
-                      <img className="medium" src={product.image} alt={product.name} />
-                    </a>
-                    <div className="card-body">
-                      <a href={`/product/${product._id}`}>
-                        <h2>{product.name}</h2>
-                      </a>
-                      <div className="rating">
-                        <span> <i className="fa fa-star" /> </span>
-                        <span> <i className="fa fa-star" /> </span>
-                        <span> <i className="fa fa-star" /> </span>
-                        <span> <i className="fa fa-star" /> </span>
-                        <span> <i className="fa fa-star" /> </span>
-                      </div>
-                      <div className="price">{product.price}</div>
-                    </div>
-                  </div>
+                  <Product product={product} />
                 ))
               }
             </div>
