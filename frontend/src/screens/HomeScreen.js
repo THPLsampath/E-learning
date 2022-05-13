9f2c6fad176f298aeffd50d8ecf970fed5952f8a
THPLsampath <sampaththpl@gmail.com>
2022-05-13 14:25:14 +0530

    impliment

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Product from '../components/Product'
-import data from '../data'
+import axios from 'axios';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
+  const [products, setProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(()=>{
+    const fetchData = async()=>{
+      try {
+        setLoading(true);
+        const {data} = await axios.get("/api/products");
+        setLoading(false);
+        setProduct(data);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+    fetchData();
+  },[])
+
   return (
     <div>
-        <div className="row center">
-        {
-            data.products.map(product=>(
-                <Product product={product} />
-            ))
-        }
-        </div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+      <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+      <div className="row center">
+      {
+          products.map(product=>(
+              <Product product={product} />
+          ))
+      }
+      </div>
+      )
+      }
     </div>
   )
 }
